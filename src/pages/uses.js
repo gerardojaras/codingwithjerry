58b5ee2301643a2f3aaaf14821ecb5e307c63fcd
gerardojaramillo <gerardo.jaramillo@gmail.com>
2020-02-27 18:05:59 -0600

    added page of uses of

---
+++ src/pages/uses.js
@@ -0,0 +1,35 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { graphql } from "gatsby"
+
+const Uses = ({data}) => {
+  const page = data.allWordpressPage.edges[0].node;
+  console.log(page);
+  return(
+    <Layout>
+    <SEO title="Gerardo Jaramillo Uses" />
+      <div dangerouslySetInnerHTML={{ __html: page.content }} />
+  </Layout>)
+}
+
+export default Uses
+
+export const query = graphql`
+  {
+  allWordpressPage(filter: {id: {eq: "fc6cd7b4-1ed2-5367-bbc0-7c879713c099"}}) {
+    edges{
+      node {
+        id
+        content
+        excerpt
+        date
+        id
+        author {
+          name
+        }
+      }
+    }
+  }
+}
+`
